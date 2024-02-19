29422f42214b48398df97f445e74ce7d84e84429
barentain <valentinemchunu11@gmail.com>
2024-02-19 08:25:52 -0800

    fix inherit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Return instance of a specific class."""
+"""Defines a class-checking function."""
 
 
 def is_same_class(obj, a_class):
-    """Check if an object is an instance of a given class.
+    """Check if an object is exactly an instance of a given class.
+
     Args:
         obj (any): The object to check.
-        a_class (type): The class to compare the type of obj to.
+        a_class (type): The class to match the type of obj to.
     Returns:
-        Boolean of an instance of a_class - True.
+        If obj is exactly an instance of a_class - True.
         Otherwise - False.
     """
     if type(obj) == a_class:
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Defines an inherited class-checking method."""
+"""Defines an inherited class-checking function."""
 
 
 def inherits_from(obj, a_class):
     """Checks if an object is an inherited instance of a class.
+
     Args:
         obj (any): The object to check.
-        a_class (type): The class to compare.
+        a_class (type): The class to match the type of obj to.
     Returns:
-        A boolean of inheritance.
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
